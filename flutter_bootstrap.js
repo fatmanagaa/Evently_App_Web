738ee8bab2e30cc69c81d2ccfcc67c5cbe3368f9
PC-ORBIT <PC-ORBIT@DESKTOP-IK6P0R2>
2026-05-11 03:31:24 +0300

    Fix: Final Web Build with Firebase Support

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "747497124" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2083793454" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
